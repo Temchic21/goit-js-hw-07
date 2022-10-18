28cf6455785ac13e3383f61b684a9fe4a9eebee4
Temchic21 <barsiccc@gmail.com>
2022-10-18 13:48:30 +0300

    commit-3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
 
+
 const galleryList = document.querySelector(".gallery");
 const galleryMarkup = createGallerySmallPictureCard(galleryItems);
 let instance;
-galleryList.innerHTML = galleryMarkup;
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 galleryList.addEventListener("click", onGalleryImageClick);
+
 function createGallerySmallPictureCard(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -23,14 +26,20 @@ function createGallerySmallPictureCard(galleryItems) {
 }
 function onGalleryImageClick(e) {
   e.preventDefault();
+  
+  if (!e.target.classList.contains('gallery__image')) {
+    return; 
+  };
   let galleryImages = e.target.dataset.source;
+  
   instance = basicLightbox.create(`<img src="${galleryImages}" width="800" height="600">`);
   instance.show();
-  galleryList.addEventListener("keydown", onEscKeyPress);
-  function onEscKeyPress(e) {
-    if (e.key === "Escape") {
-      instance.close(() => console.log(""));
-      galleryList.removeEventListener("keydown", onEscKeyPress);
+
+    galleryList.addEventListener("keydown", onEscKeyPress);
+    function onEscKeyPress(e) {
+      if (e.key === "Escape") {
+        instance.close(() => console.log("только один раз сработает когда модалки нет не работает"));
+        galleryList.removeEventListener("keydown", onEscKeyPress);
+      }
     }
-  }
 };
