6a0a97801911f5c61a58319cddf876f29cf11f63
Temchic21 <barsiccc@gmail.com>
2022-10-18 21:02:35 +0300

    commit-4

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,58 @@
-import { galleryItems } from "./gallery-items.js";
-
-const galleryList = document.querySelector(".gallery");
-const galleryMarkup = createGallerySmallPictureCard(galleryItems);
-let instance;
-
-galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-galleryList.addEventListener("click", onGalleryImageClick);
-
-function createGallerySmallPictureCard(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-        />
-        </a>
-        </div>`;
-    })
-    .join("");
+import { galleryItems } from "./gallery-items.js"; 
+ 
+const galleryList = document.querySelector(".gallery"); 
+const galleryMarkup = createGallerySmallPictureCard(galleryItems); 
+ 
+let instance; 
+ 
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup); 
+galleryList.addEventListener("click", onGalleryImageClick); 
+ 
+function createGallerySmallPictureCard(galleryItems) { 
+  return galleryItems 
+    .map(({ preview, original, description }) => { 
+      return `<div class="gallery__item"> 
+        <a class="gallery__link" href="${original}"> 
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        data-source="${original}" 
+        alt="${description}" 
+        /> 
+        </a> 
+        </div>`; 
+    }) 
+    .join(""); 
+} 
+ 
+function onGalleryImageClick(event) { 
+  event.preventDefault(); 
+ 
+  if (!event.target.classList.contains("gallery__image")) return; 
+ 
+  let galleryImages = event.target.dataset.source; 
+ 
+  instance = basicLightbox.create( 
+    `<img src="${galleryImages}" width="800" height="600">`, 
+    { 
+      onShow: () => { 
+        console.log("Добавили ESC"); 
+        document.addEventListener("keydown", onEscKeyPress); 
+      }, 
+      onClose: () => { 
+        console.log("Убрали ESC"); 
+        document.removeEventListener("keydown", onEscKeyPress); 
+      }, 
+    } 
+  ); 
+ 
+  instance.show(); 
+} 
+ 
+function onEscKeyPress(event) { 
+  if (event.key === "Escape") { 
+    instance.close(() => { 
+      console.log("Закрыли, когда нажали ESC"); 
+    }); 
+  } 
 }
-function onGalleryImageClick(e) {
-  e.preventDefault();
-  
-  if (!e.target.classList.contains('gallery__image')) {
-    return; 
-  };
-  let galleryImages = e.target.dataset.source;
-  
-  instance = basicLightbox.create(`<img src="${galleryImages}" width="800" height="600">`);
-  instance.show();
-  galleryList.addEventListener("keydown", onEscKeyPress);
-};
-function onEscKeyPress(e) {
-      if (e.key === "Escape") {
-        instance.close(() => console.log("только один раз сработает когда модалки нет не работает"));
-        galleryList.removeEventListener("keydown", onEscKeyPress);
-      }
-    }
